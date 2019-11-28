26e465c7ad36957821daf532438333fde8d8e13e
Muhammad Talat Mahmud <36367916+MdTalatMahmud@users.noreply.github.com>
2019-11-28 23:38:08 +0600

    user registration
    user registration using asp dot net

---
+++ UserRegistration/UserRegistration/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UserRegistration.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(Table U)
+        {
+            if (ModelState.IsValid)
+            {
+                using (MyDatabaseEntities de=new MyDatabaseEntities())
+                {
+                    de.Tables.Add(U);
+                    de.SaveChanges();
+                    ModelState.Clear();
+                    U = null;
+                    ViewBag.Message = "Successfully user created!";
+                }
+            }
+
+            return View(U);
+        }
+    }
+}
